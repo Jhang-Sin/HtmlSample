aeb7a398bde17570d5aeed87ab04dd8140eddf72
Jhang-Yong-Sin <z54397@gmail.com>
2023-08-01 11:26:58 +0800

    調整JS

--- JS/SinData.js
+++ JS/SinData.js
@@ -1,7 +1,23 @@
 "use strict";
 
-    function DataOutPut()
-    {
-        console.log('you targe OutPutJS')
+function SWDT()
+            {
+               var  year= "00";
+               var mon= "00";
+               var Day="00";
+               var HH = "00";
+               var MM = "00";
+               var SS = "00";                
+                
 
-    }
+                var DT = new Date;
+                year = DT.getFullYear();///取年
+                mon = (DT.getMonth()+1);///取月///+1修正月份
+                Day = (DT.getDate());///取日
+                HH = DT.getHours(); ///取時
+                MM = DT.getMinutes();///取分
+                SS = DT.getSeconds();//取秒
+
+                alert(year +"年"+mon+"月"+Day+"日 "+HH+":"+MM+":"+SS+"秒");
+
+            };
