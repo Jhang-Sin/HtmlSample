366d9230f24799f793a4b886e33f3df87eec67ee
Jhang-Yong-Sin <z54397@gmail.com>
2023-12-21 09:42:23 +0800

    modifyCSS
    圓形/圓角按鈕CSS測試

---
+++ JS/Notes.js
@@ -0,0 +1,20 @@
+"use strict";
+function Notes()
+{
+
+    document.getElementById("notesDay").innerText="";
+    document.getElementById("notesDay").innerHTML = "2023-12-21 modify";}
+
+
+let AllNum = 0;///宣告全域變數[let + 變數名]
+///let 可能會被重新指定值
+
+const Allnum2 = 99;
+///// const 不會被重新指定值
+
+/*
+建議在開發ES6之後的專案中，使用“ const ”或者“let”嚴謹的宣告變數而非“var”，
+除了讓開發的專案更加穩定外，也可以增加程式碼的可讀性。例如：“ const ”可以提醒閱讀程式碼的人，
+此變數不應該去做改變。
+
+*/
