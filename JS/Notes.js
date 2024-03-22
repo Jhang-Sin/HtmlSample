c53149bd89ff1c3284a986682b61bcebddf2436c
Jhang-Yong-Sin <z54397@gmail.com>
2024-03-22 11:00:46 +0800

    AddReactPage

--- JS/Notes.js
+++ JS/Notes.js
@@ -3,7 +3,7 @@ function Notes()
 {
 
     document.getElementById("notesDay").innerText="";
-    document.getElementById("notesDay").innerHTML = "2024-3-19 異動/更新";}
+    document.getElementById("notesDay").innerHTML = "2024-3-22 異動/更新";}
 
 
 let AllNum = 0;///宣告全域變數[let + 變數名]
