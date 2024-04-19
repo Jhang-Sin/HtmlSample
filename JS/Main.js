36c94acd5c00ce14164bc30a768e1ccf7b35a904
Jhang-Yong-Sin <z54397@gmail.com>
2024-04-19 10:42:50 +0800

    收合效果CSS

--- JS/Main.js
+++ JS/Main.js
@@ -3,3 +3,10 @@ function TEST()
 {
     console.log('MainJS-OK');
 }
+/*收闔效果*/ 
+$(function(){
+    $(".flip").click(function(){
+        $(".panel").slideToggle("slow");
+        $(".xs1").toggle();
+        $(".xs2").toggle();
+      });});
