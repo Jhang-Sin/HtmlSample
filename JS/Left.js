13e418087ca0cd9ff0f386410506532a69be6417
Jhang-Yong-Sin <z54397@gmail.com>
2024-05-14 11:39:45 +0800

    Add-LocalAngJSfile
    angJs-1.8.2

--- JS/Left.js
+++ JS/Left.js
@@ -3,3 +3,11 @@ function TEST()
 {
     console.log('LeftJS-OK');
 }
+
+///呼叫主視窗事件///
+function MainEvent()
+{
+    console.log('LeftJS-MainEvent-')
+    var MainObj = document.getElementById('MMD');
+    MainObj.wrDiv('OH ~ Left');
+}
--- JS/Main.js
+++ JS/Main.js
@@ -3,6 +3,7 @@ function TEST()
 {
     console.log('MainJS-OK');
 }
+
 /*收闔效果*/ 
 $(function(){
     $(".flip").click(function(){
@@ -10,3 +11,14 @@ $(function(){
         $(".xs1").toggle();
         $(".xs2").toggle();
       });});
+
+
+/*將接收寫在畫面的DIV中 */
+
+      function wrDiv(SubStr)
+      {   
+          var ShowOBJ = document.getElementById("MMD");                
+          ShowOBJ.innerHTML += SubStr;
+
+      } 
+      
