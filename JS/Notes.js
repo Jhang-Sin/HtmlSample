9aa02034fe108820a8224b520f02f9c58d87d228
Jhang-Yong-Sin <z54397@gmail.com>
2024-08-02 12:27:37 +0800

    AD廣告測試
    AD廣告測試

--- JS/Notes.js
+++ JS/Notes.js
@@ -3,7 +3,7 @@ function Notes()
 {
 
     document.getElementById("notesDay").innerText="";
-    document.getElementById("notesDay").innerHTML = "2024-7-30 異動/更新";}
+    document.getElementById("notesDay").innerHTML = "2024-8-2 異動/更新";}
 
 
 let AllNum = 0;///宣告全域變數[let + 變數名]
