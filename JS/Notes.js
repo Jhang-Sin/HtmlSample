4bc8ad940316baf8f69297b368a19bc7003b522f
Jhang-Yong-Sin <z54397@gmail.com>
2025-05-22 11:23:51 +0800

    Update Notes.js

--- JS/Notes.js
+++ JS/Notes.js
@@ -3,7 +3,7 @@ function Notes()
 {
 
     document.getElementById("notesDay").innerText="";
-    document.getElementById("notesDay").innerHTML = "2025-05-21異動/更新";
+    document.getElementById("notesDay").innerHTML = "2025-05-22異動/更新";
 
 }
 
