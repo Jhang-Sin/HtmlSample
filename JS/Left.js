7d9ea394de1284cce41e9296dd0285e8dee54c50
Jhang-Yong-Sin <z54397@gmail.com>
2025-07-16 10:32:29 +0800

    Update Left.js

--- JS/Left.js
+++ JS/Left.js
@@ -7,7 +7,7 @@ function TEST()
 ///呼叫主視窗事件///
 function MainEvent()
 {
-    console.log('LeftJS-MainEvent-')   
+    console.log('LeftJS-MainEvent--')   
     var MainObj= document.getElementById("MMD")
     ///事件觸發用 物件.click()
     MainObj.click()  ///結尾不用加上分號
